a52a677a752c6a8e606eb3450ffc78391039bd9b
dinaminahi <46566190+dinaminahi@users.noreply.github.com>
2019-11-14 22:11:16 +0200

    Create day-32.js

---
+++ day-32.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/this-is-a-problem/train/javascript
+
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = first + ' ' + last;
+}
+
+
+
+//https://www.codewars.com/kata/this-is-an-other-problem
+
