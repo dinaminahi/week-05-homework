c820f32b4ca6d99dd2461c161d81aa52d7fc8565
dinaminahi <46566190+dinaminahi@users.noreply.github.com>
2019-11-17 17:54:32 +0200

    Update day-33.js

--- day-33.js
+++ day-33.js
@@ -445,3 +445,45 @@ describe('use `import` to import functions that have been exported (somewhere el
 });
 
 
+//https://tddbin.com/?664#?kata=es6/language/modules/import
+
+// 61: modules - import
+// To do: make all tests pass, leave the assert lines unchanged!
+import assert from 'assert'; //import an entire module
+import {equal} from 'assert';  //import a single member
+import {deepEqual, notEqual} from 'assert'; //separate multiple members with a comma
+import {equal as myEqual} from 'assert'; //using member as alias as memberName
+import {default as myAssert} from 'assert';//rename the default export of a module, using default as alias as memberName
+describe('use import to import functions that have been exported (somewhere else)', function() {
+ describe('the import statement', function() {
+   it('is only allowed on the root level', function() {
+     // Try to comment this out, it will yell at you!
+     // import assert from 'assert';
+   });
+   it('import an entire module using import <name> from "<moduleName>"', function() {
+     const expectedType = 'function';
+     assert.equal(typeof assert, expectedType);
+   });
+ });
+ describe('import members', function() {
+   it('import a single member, using import {<memberName>} from "module"', function() {
+     assert.strictEqual(equal, assert.equal);
+   });
+   describe('separate multiple members with a comma', function() {
+     it('`deepEqual` from the assert module', () => {
+       assert.strictEqual(deepEqual, assert.deepEqual);
+     });
+     it('`notEqual` from the assert module', () => {
+       assert.strictEqual(notEqual, assert.notEqual);
+     });
+   });
+ });
+ describe('alias imports', function() {
+   it('using member as alias as memberName', function() {
+     assert.strictEqual(myEqual, assert.equal);
+   });
+   it('rename the default export of a module, using default as alias as memberName', function() {
+     assert.strictEqual(myAssert, assert);
+   });
+ });
+});
